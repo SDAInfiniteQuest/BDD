a19f4d266fe5b346f40d9b9e4f5ff9d54dafd920
Pallamidessi <joseph.pallamidessi@gmail.com>
2013-04-01 15:57:10 +0200

    site: structure html et premier script

---
+++ site/footer.php
@@ -0,0 +1,25 @@
+<footer>
+		
+	<div id="nav_and_search_header">
+		<nav/>
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Terms</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		<nav>
+		<div id="searchbox_header">
+			<form method="post" action="#">
+				<p>
+					<label for="search_footer">Search</label> <input type="text"name="search2" id="search_footer">
+				<p>
+		</div>
+	
+	</div>
+
+	<div id="copyright">
+		<p>BibliMedia,site realiser dans le cadre du cours de BDD2</p>
+		<p>Par Simon Andreux et Joseph Pallamidessi</p>
+	</div>
+
+</footer>
---
+++ site/header.php
@@ -0,0 +1,26 @@
+<header>
+	<div id="header_image">
+		<h1>BiblioMedia</h1>
+	</div>
+	
+	<div id="nav_and_search_header">
+		<nav/>
+			<ul>
+				<li><a href="#">Sign-in</a></li>
+				<li><a href="#">Create Account</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">List</a></li>
+				<li><a href="#">People</a></li>
+			</ul>
+		<nav>
+		
+		<div id="searchbox_header">
+			<form method="post" action="#">
+				<p>
+					<label for="search_header">Search</label> <input type="text" name="search" id="search_header">
+				</p>
+		</div>
+	
+	</div>
+</header>
+
